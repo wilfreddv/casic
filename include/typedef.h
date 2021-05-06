cb27091e8763bacd87f80f340ae591bbfb92aa9b
wilfreddv <hilgertjuuhbosgewoon@hotmail.com>
2021-05-06 18:55:17 +0200

    Extract typedefs

---
+++ include/typedef.h
@@ -0,0 +1,11 @@
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+
+
+typedef struct Token Token;
+typedef struct TokenStream TokenStream;
+typedef enum TokenType TokenType;
+typedef enum KeywordType KeywordType;
+
+
+#endif // TYPEDEF_H
